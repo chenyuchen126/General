9fabdca379c089975fdec7d24be2e22752098d13
ChrisChen <chenttm6@gmail.com>
2022-09-23 20:39:34 +0800

    add get_key()
    just add key

--- enctry.py
+++ enctry.py
@@ -1,11 +1,15 @@
 import base64
 import getpass
+
+def get_key():
+    return "badfdfeatretq44315u3iu8aut8471409754ut94q8718495984utq94qutq984t"
+
 # enctry 
 def enctry(s):
-    k = 'abcdefg1234567890'
+    k = get_key()
     encry_str = ""
-    for i,j in zip(s,k): # i為字符，j為秘鑰字符
-        temp = str(ord(i)+ord(j))+'_' # 加密字符 = 字符的Unicode碼 + 秘鑰的Unicode碼
+    for i,j in zip(s,k): 
+        temp = str(ord(i)+ord(j))+'_' 
         encry_str = encry_str + temp
     s1 = base64.b64encode(encry_str.encode("utf-8"))
     return s1
@@ -13,10 +17,10 @@ def enctry(s):
 # dectry
 def dectry(s2):
     p = base64.b64decode(s2).decode("utf-8")
-    k = 'abcdefg1234567890'
+    k = get_key()
     dec_str = ""
-    for i,j in zip(p.split("_")[:-1],k): # i 為加密字符，j為秘鑰字符
-        temp = chr(int(i) - ord(j)) # 解密字符 = (加密Unicode碼字符 - 秘鑰字符的Unicode碼)的單字節字符
+    for i,j in zip(p.split("_")[:-1],k): 
+        temp = chr(int(i) - ord(j)) 
         dec_str = dec_str+temp
     return dec_str
 
